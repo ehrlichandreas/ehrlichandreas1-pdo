034d9b970ad9a3b09047c2b3f92d3856aba3d119
Andreas Ehrlich <ehrlich.andreas@googlemail.com>
2014-02-04 15:11:51 +0100

    abstract pdo class

---
+++ src/EhrlichAndreas/Pdo/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ * @author Ehrlich, Andreas <ehrlich.andreas@googlemail.com>
+ */
+class EhrlichAndreas_Pdo_Exception extends RuntimeException
+{
+
+    public $errorInfo; // corresponds to PDO::errorInfo()
+}
+
+?>
